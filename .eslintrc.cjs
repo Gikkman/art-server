c69ac6aa06f2ebaecd078381a00d5aba83a2a874
gikkman <gikk@gikkman.com>
2023-05-18 13:49:42 +0200

    Fix some styling and linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,7 +4,22 @@ module.exports = {
   parser: "@typescript-eslint/parser",
   plugins: ["@typescript-eslint", "prettier"],
 
-  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "prettier"],
+  parserOptions: {
+    project: "app/frontend/tsconfig.json",
+    extraFileExtensions: [".svelte"],
+  },
+  overrides: [
+    {
+      files: ["*.svelte"],
+      parser: "svelte-eslint-parser",
+      // Parse the `<script>` in `.svelte` as TypeScript by adding the following configuration.
+      parserOptions: {
+        parser: "@typescript-eslint/parser",
+      },
+    },
+  ],
+
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:svelte/recommended", "prettier",],
 
   rules: {
     "@typescript-eslint/no-unused-vars": [
