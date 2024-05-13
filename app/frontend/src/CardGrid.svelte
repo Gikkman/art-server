2270a1012343bb8c40e453b1d3665bb1bebe1b18
gikkman <gikk@gikkman.com>
2024-05-13 23:06:13 +0200

    Tweek css a bit more

--- app/frontend/src/CardGrid.svelte
+++ app/frontend/src/CardGrid.svelte
@@ -16,7 +16,7 @@
   .card-grid {
     padding-top: 25px;
     display: grid;
-    grid-template-columns: repeat(auto-fill, minmax(220px, 1fr));
+    grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
     gap: 1rem;
   }
   .sneaky-preload {
